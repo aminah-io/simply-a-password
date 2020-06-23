27322af435dd2d280646671464ccc2391348a2ec
Aminah Burch <hello@aminah.io>
2020-06-23 10:47:25 -0700

    Initial commit

--- Simply A Password/ViewController.swift
+++ Simply A Password/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    let characters = ["a", "b", "C", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "X", "T", "U", "V", "W", "X", "Y", "Z", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+
+    var password = "\(characters.randomElement() ?? <#default value#>)\(characters.randomElement() ?? <#default value#>)\(characters.randomElement() ?? <#default value#>)\(characters.randomElement() ?? <#default value#>)\(characters.randomElement() ?? <#default value#>)\(characters.randomElement() ?? <#default value#>)\(characters.randomElement() ?? <#default value#>)\(characters.randomElement() ?? <#default value#>)\(characters.randomElement() ?? <#default value#>)\(characters.randomElement() ?? <#default value#>)\(characters.randomElement() ?? <#default value#>)\(characters.randomElement() ?? <#default value#>)\(characters.randomElement() ?? <#default value#>)"
 
 
 }
