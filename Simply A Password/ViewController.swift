c4ac0a039db04f6e68201ad5f749033fd16dd774
Aminah Burch <hello@aminah.io>
2020-06-23 10:31:02 -0700

    Initial Commit

---
+++ Simply A Password/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simply A Password
+//
+//  Created by Aminah Jamil on 6/23/20.
+//  Copyright © 2020 Aminah Jamil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
